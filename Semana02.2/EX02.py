f9d50c43541ced6341e378e9c29d9ede9de9f38c
talissonbugsf <talissonbugsf@gmail.com>
2026-08-18 18:10:33 -0300

    Create EX02.py

---
+++ Semana02.2/EX02.py
@@ -0,0 +1,85 @@
+class No:
+    def __init__(self, dado):
+        self.dado = dado
+        self.proximo = None
+
+def menu():
+    print("1 - inserir item.")
+    print("2 - listar itens.")
+    print("3 - Retirar Itens.")
+    print("4 - Maiores que 'n'")
+    print("5 - Sair.")
+    opcao = int(input("Digite a opção:"))
+    return opcao
+
+def inserir(lista, dado):
+    no = No(dado)
+
+    if lista == None:
+        lista = no
+        return lista
+    
+    no.proximo = lista
+    lista = no
+    return lista
+
+def listar(lista):
+    aux = lista
+    while aux != None:
+        print(" - ", aux.dado)
+        aux = aux.proximo
+
+def remover(lista, dado):
+    aux = lista
+    anterior = None
+
+    if lista == None:
+        print("Lista vazia.")
+        return lista
+ 
+    while aux != None:
+        if aux.dado == dado:
+            if aux == lista:
+                lista = lista.proximo
+                return lista
+            else:
+                anterior.proximo = aux.proximo
+                return lista
+        anterior = aux
+        aux = aux.proximo
+    print("Dado não encontrado.")
+    return lista
+
+def maiores_n(lista, n):
+    aux = lista
+    maiores = 0
+
+    if lista == None:
+        print("Lista vazia")
+        return lista
+    while aux != None:
+        if aux.dado > n:
+            maiores += 1
+        aux = aux.proximo
+    print(f"Dados maiores que {n} {maiores}.")
+
+def main():
+    lista = None
+    opc = 0
+    n = float(input("Digite um valor para 'n':"))
+
+    while opc != 4:
+        opc = menu()
+        if opc == 1:
+            dado = float(input("Digite um dado:"))
+            lista = inserir(lista,dado)
+        elif opc == 2:
+            listar(lista)
+        elif opc == 3:
+            dado = float(input("Dado parar retirar:"))
+            lista = remover(lista, dado)
+        elif opc == 4:
+            maiores_n(lista, n)
+
+
+main()
