8e8564f673591d2cf6fab641fa8cb0c3988fade4
talissonbugsf <talissonbugsf@gmail.com>
2026-08-18 19:05:12 -0300

    Create EX04.py

---
+++ Semana02.2/EX04.py
@@ -0,0 +1,82 @@
+class No:
+    def __init__(self, dado):
+        self.dado = dado
+        self.proximo = None
+
+def menu():
+    print("1 - inserir item.")
+    print("2 - listar itens.")
+    print("3 - Retirar Itens.")
+    print("4 - Inserir no final.")
+    print("5 - Sair.")
+    opcao = int(input("Digite a opção:"))
+    return opcao
+
+def inserir(lista, dado):
+    no = No(dado)
+
+    if lista == None:
+        lista = no
+        return lista
+    
+    no.proximo = lista
+    lista = no
+    return lista
+
+def listar(lista):
+    aux = lista
+    while aux != None:
+        print(" - ", aux.dado)
+        aux = aux.proximo
+
+def remover(lista, dado):
+    aux = lista
+    anterior = None
+
+    if lista == None:
+        print("Lista vazia.")
+        return lista
+ 
+    while aux != None:
+        if aux.dado == dado:
+            if aux == lista:
+                lista = lista.proximo
+                return lista
+            else:
+                anterior.proximo = aux.proximo
+                return lista
+        anterior = aux
+        aux = aux.proximo
+    print("Dado não encontrado.")
+    return lista
+
+def lista_insere_final(lista, valor):
+    no = No(valor)
+    if lista == None:
+        return no
+    aux = lista
+    anteriro = None
+    while aux != None:
+        anterior = aux
+        aux = aux.proximo
+    anterior.proximo = no
+    return lista
+
+def main():
+    lista = None
+    opc = 0
+
+    while opc != 4:
+        opc = menu()
+        if opc == 1:
+            dado = float(input("Digite um dado:"))
+            lista = inserir(lista,dado)
+        elif opc == 2:
+            listar(lista)
+        elif opc == 3:
+            dado = float(input("Dado parar retirar:"))
+            lista = remover(lista, dado)
+        elif opc == 4:
+            valor = float(input("Digite um dado:"))
+            lista_insere_final(lista, valor)
+main()
