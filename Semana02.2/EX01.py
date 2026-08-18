6153c736f7afb4033b0e9f95792fb6f0829d48b4
talissonbugsf <talissonbugsf@gmail.com>
2026-08-18 17:41:46 -0300

    Create EX01.py

---
+++ Semana02.2/EX01.py
@@ -0,0 +1,72 @@
+class No:
+    def __init__(self, dado):
+        self.dado = dado
+        self.proximo = None
+
+def menu():
+    print("1 - inserir item.")
+    print("2 - listar itens.")
+    print("3 - Buscar Itens.")
+    print("4 - Retirar Itens.")
+    print("5 - Sair.")
+    opcao = int(input("Digite a opção:"))
+    return opcao
+
+def inserir(lista, dado):
+    no = No(dado)
+
+    if lista == None:
+        lista = no
+        return lista
+    
+    no.proximo = lista
+    lista = no
+    return lista
+
+def listar(lista):
+    aux = lista
+    while aux != None:
+        print(" - ", aux.dado)
+        aux = aux.proximo
+
+def remover(lista, dado):
+    aux = lista
+    anterior = None
+
+    if lista == None:
+        print("Lista vazia.")
+        return lista
+ 
+    while aux != None:
+        if aux.dado == dado:
+            if aux == lista:
+                lista = lista.proximo
+                return lista
+            else:
+                anterior.proximo = aux.proximo
+                return lista
+        anterior = aux
+        aux = aux.proximo
+    print("Dado não encontrado.")
+    return lista
+
+def main():
+    lista = None
+    opc = 0
+
+    while opc != 5:
+        opc = menu()
+        if opc == 1:
+            dado = float(input("Digite um dado:"))
+            lista = inserir(lista,dado)
+        elif opc == 2:
+            listar(lista)
+        elif opc == 3:
+            dado = float(input("Dado para buscar:"))
+            buscar(lista, dado)
+        elif opc == 4:
+            dado = float(input("Dado parar retirar:"))
+            lista = remover(lista, dado)
+
+
+main()
