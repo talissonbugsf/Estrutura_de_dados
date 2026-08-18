acb72e2b0e3074b58017fe7b9149b6200c75e9f3
talissonbugsf <talissonbugsf@gmail.com>
2026-08-18 20:12:31 -0300

    Update EX05.py

--- Semana02.2/EX05.py
+++ Semana02.2/EX05.py
@@ -1,1 +1,82 @@
+class No:
+    def __init__(self, dado):
+        self.dado = dado
+        self.proximo = None
 
+def menu():
+    print("1 - inserir item.")
+    print("2 - listar itens.")
+    print("3 - Retirar Itens.")
+    print("4 - Calcular a média.")
+    print("5 - Sair.")
+    opcao = int(input("Digite a opção:"))
+    return opcao
+
+def inserir(lista, dado):
+    no = No(dado)
+
+    if lista == None:
+        lista = no
+        return lista
+    
+    no.proximo = lista
+    lista = no
+    return lista
+
+def listar(lista):
+    aux = lista
+    while aux != None:
+        print(" - ", aux.dado)
+        aux = aux.proximo
+
+def remover(lista, dado):
+    aux = lista
+    anterior = None
+
+    if lista == None:
+        print("Lista vazia.")
+        return lista
+ 
+    while aux != None:
+        if aux.dado == dado:
+            if aux == lista:
+                lista = lista.proximo
+                return lista
+            else:
+                anterior.proximo = aux.proximo
+                return lista
+        anterior = aux
+        aux = aux.proximo
+    print("Dado não encontrado.")
+    return lista
+
+def lista_calcula_media(lista):
+    if lista == None:
+        print("Lista vazia.")
+        return
+    aux = lista
+    valores = []
+    while aux != None:
+        valores.append(aux.dado)
+        aux = aux.proximo
+    total = sum(valores)
+    media = total / len(valores)
+    print(f"Média dos dados:{media}")
+        
+def main():
+    lista = None
+    opc = 0
+
+    while opc != 4:
+        opc = menu()
+        if opc == 1:
+            dado = float(input("Digite um dado:"))
+            lista = inserir(lista,dado)
+        elif opc == 2:
+            listar(lista)
+        elif opc == 3:
+            dado = float(input("Dado parar retirar:"))
+            lista = remover(lista, dado)
+        elif opc == 4:
+            lista_calcula_media(lista)
+main()
