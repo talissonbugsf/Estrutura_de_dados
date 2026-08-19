968290c90a1071c703d9bdeb57e2fb5eafa2a688
talissonbugsf <talissonbugsf@gmail.com>
2026-08-19 20:13:54 -0300

    Change loop condition from 4 to 5

--- Semana02.2/EX03.py
+++ Semana02.2/EX03.py
@@ -63,7 +63,7 @@ def main():
     lista = None
     opc = 0
 
-    while opc != 4:
+    while opc != 5:
         opc = menu()
         if opc == 1:
             dado = float(input("Digite um dado:"))
