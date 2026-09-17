0fc5e0fb98f36f14834d5c5a693a071e681f4f9e
talissonbugsf <talissonbugsf@gmail.com>
2026-09-17 20:45:21 -0300

    Create EX04_Duplamente_Encadeada.py

---
+++ Semana07/EX04_Duplamente_Encadeada.py
@@ -0,0 +1,59 @@
+class NoDuplo:
+    def __init__(self, nome):
+        self.nome = nome
+        self.proximo = None
+        self.anterior = None
+
+def adicionar(lista, dado):
+    novo = NoDuplo(dado)
+
+    if lista is None:
+        lista = novo
+        return lista
+
+    novo.proximo = lista
+    lista.anterior = novo
+    lista = novo
+    return lista
+
+def percorrer(lista):
+    if lista is None:
+        print("Lista vazia!")
+        return
+
+    contador = 1
+    aux = lista
+    while aux is not None:
+        print(f"{contador} - {aux.nome};")
+        contador += 1
+        aux = aux.proximo
+
+def menu():
+    print("MENU:")
+    print("1 - Inserir.")
+    print("2 - Percorrer.")
+    print("3 - Sair.")
+    opc = int(input("Digite uma opção:"))
+    return opc
+
+def main():
+    opc = 0
+    lista = None
+
+    while opc != 3:
+        try:
+            opc = menu()
+            if opc == 1:
+                dado = input("Digite o nome do atleta:")
+                lista = adicionar(lista, dado)
+            elif opc == 2:
+                percorrer(lista)
+            elif opc == 3:
+                print("Até mais!")
+            else:
+                print("Tenta denovo!")
+
+        except ValueError:
+            print("Digita certo...")
+
+main()
