7419fe2e845ea75817dd6bcddd613ab762efd459
talissonbugsf <talissonbugsf@gmail.com>
2026-09-18 14:25:12 -0300

    Create EX12_Fila.py

---
+++ Semana07/EX12_Fila.py
@@ -0,0 +1,76 @@
+class Fila:
+    def __init__(self, usuarios):
+        self.usuarios = usuarios
+        self.proximo = None
+        self.anterior = None
+
+def enfileirar(fila_inicio, fila_fim, usuario):
+    novo = Fila(usuario)
+
+    if fila_inicio is None:
+        fila_inicio = novo
+        fila_fim = novo
+        return fila_inicio, fila_fim
+
+    fila_fim.proximo = novo
+    novo.anterior = fila_fim
+    fila_fim = novo
+    return fila_inicio, fila_fim
+
+def desenfileirar(fila_inicio, fila_fim):
+    if fila_inicio is None:
+        print("Lista vazia!")
+        return None, None
+
+    if fila_inicio == fila_fim:
+        print("Único elemento na lista!")
+        return None, None
+
+    fila_inicio = fila_inicio.proximo
+    fila_inicio.anterior = None
+    return fila_inicio, fila_fim
+
+def percorrer(fila_inicio):
+    if fila_inicio is None:
+        print("Fila vazia!")
+        return
+
+    aux = fila_inicio
+    while aux is not None:
+        print(f"- {aux.usuarios};")
+        aux = aux.proximo
+
+def menu():
+    print("\nMENU:")
+    print("1 - Enfileirar.")
+    print("2 - Desenfileirar.")
+    print("3 - Percorrer.")
+    print("4 - Sair.")
+
+    opc = int(input("Digite uma opção:"))
+    return opc
+
+def main():
+    opc = 0
+    fila_inicio = None
+    fila_fim = None
+
+    while opc != 4:
+        try:
+            opc = menu()
+            if opc == 1:
+                usuario = input("Digite seu nome:").title()
+                fila_inicio, fila_fim = enfileirar(fila_inicio, fila_fim, usuario)
+            elif opc == 2:
+                fila_inicio, fila_fim = desenfileirar(fila_inicio, fila_fim)
+            elif opc == 3:
+                percorrer(fila_inicio)
+            elif opc == 4:
+                print("Até mais!")
+            else:
+                print("Tenta denovo!")
+
+        except ValueError:
+            print("Digita certo...")
+
+main()
